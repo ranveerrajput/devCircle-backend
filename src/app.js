ca4842d0a0e7dd8d0a20e90cc1e7ff971ccc2141
ranveerrajput <ranveerrajput351@gmail.com>
2024-11-27 22:17:25 +0530

    advance rouotes and patterns

--- src/app.js
+++ src/app.js
@@ -2,20 +2,38 @@ const express = require('express');
 const app = express();
 
 
-app.use("/test",(req,res)=>{
-    res.send("From the test route");
 
-})
+//GET Request
+app.get('/test', (req,res)=>{
+    console.log('From the GET API');
+    res.send("Data send successfully")
+});
 
-app.use("/hello", (req,res)=>{
-    res.send("Hello hello hello");
-})
+//POST Request
+app.post('/test', (req,res)=>{
+    console.log('From the POST API');
+    res.send("Data added successfully")
+});
 
-app.use("/",(req,res)=>{
-    res.send("Welcome to the Home Page");
-})
+//advace routing
+
+ app.get("/ab?c", (req,res)=>{
+    console.log("This route acces path with or without b");
+    res.send("Data send successfully");
+ });
+
+ app.get("/ab+c", (req,res)=>{
+    console.log("This route acces path with satrting as a and ending with c and any times b");
+    res.send("Data send successfully from ab+c");
+ });
+
+ app.get("/ab*c", (req,res)=>{
+    console.log("This route acces path with satrting as ab and ending with c and any values in between them");
+    res.send("Data send successfully from ab*c");
+ });
 
 
 app.listen(7777,()=>{
     console.log("Server is successfully running on the port 7777.....");
 });
+
