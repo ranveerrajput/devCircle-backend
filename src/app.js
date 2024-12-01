e6632afb2ae800e47a31d6e48000f16c33332206
ranveerrajput <ranveerrajput351@gmail.com>
2024-12-01 10:25:26 +0530

    Create the USER schema and Model

--- src/app.js
+++ src/app.js
@@ -1,24 +1,14 @@
 const express = require('express');
 const app = express();
 const {connectDB} = require("./config/database");
-const {adminAuth} = require("./middleware/auth");
+// const {adminAuth} = require("./middleware/auth");
 
-app.use("/admin", adminAuth);
-
-app.get("/admin/getAllData", (req,res)=>{
-    //check user is admin or not
-    console.log("from /getAllData");
-    res.send("Send all the user data successfully");
-})
-
-app.delete("/admin/deleteData", (req,res)=>{
-    //check user is admin or not
-    console.log("from /deleteData")
-    res.send("Record deleted successfully");
-})
+// app.use('/signUp', (,res)=>{
+//     const user = 
+// })
 
 connectDB().then(()=>{
-    console.log("Data Base Connected Sussfully");
+    console.log("Data Base Connected Sucesssfully");
     app.listen("7777", ()=>{console.log("Serve is successfully listening on port 7777")})
 }).catch(()=>{
     console.log("Database can not be connectded");
